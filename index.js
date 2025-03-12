49d82bd741575188cf3ac962231bc1f4c9fa4e16
moreen karimi <moreenk541@gmail.com>
2025-03-12 13:25:18 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers =function(drivers){
+return drivers.slice(0,2)
+  
+}
+
+   
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+const returnLastTwoDrivers=function(LastTwoDrivers){
+    return LastTwoDrivers.slice(-2)
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+console.log(selectingDrivers[0](['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+console.log(selectingDrivers[1](['Antonia', 'Nuru', 'Amari', 'Mo']));
+function createFareMultiplier(num1){
+    return function(fare){
+        return fare*num1
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(2)) 
+
+const fareTripler =createFareMultiplier(3)
+console.log(fareTripler(9)
+)
+
+const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+};
+
+
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers));
+
+
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers));
